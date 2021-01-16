80e1fffbc399689d8bfbfa457782ff3721852f42
Diana Shchegoleva <dschegoleva@gmail.com>
2021-01-16 11:24:16 +0300

    Add model with specs

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  it 'validates presence of title' do
+    expect(Question.new(body: 'body')).to_not be_valid
+  end
+
+  it 'validates presence of body' do
+    expect(Question.new(title: 'Title')).to_not be_valid
+  end
+end
