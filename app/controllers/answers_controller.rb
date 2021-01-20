a755b36d1f354e4162c3a737ba32e1447467ed75
Diana Shchegoleva <dschegoleva@gmail.com>
2021-01-20 19:36:43 +0300

    Add show action

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,4 +2,12 @@ class AnswersController < ApplicationController
   def index
     @answers = Answer.all
   end
+
+  private
+
+  def answer
+    @answer ||= params[:id] ? Answer.find(params[:id]) : Answer.new
+  end
+
+  helper_method :answer
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+  
   describe 'GET #index' do
     let(:answers) { create_list(:answer, 5) }
-    let(:question) { create(:question) }
+
+    before { get :index, params: { question_id: question } }
 
     it 'populates an array of all answers' do
-      get :index, params: { question_id: question }
       expect(assigns(:answers)).to match_array(answers)
     end
     it 'renders index view' do
-      get :index, params: { question_id: question }
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:answer) { create(:answer) }
+
+    it 'renders show view' do
+      get :show, params: { id: answer, question_id: question }
+      expect(response).to render_template :show
     end
   end
 end
