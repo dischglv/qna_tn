1f5aa589ad9217590c3ed116b45757d09b67bc54
Diana Shchegoleva <dschegoleva@gmail.com>
2021-02-05 13:38:04 +0300

    Add specs for owes_resource method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe 'method owes_resource' do
+    let(:user) { create(:user) }
+
+    it "compares valid user with resource owner" do
+      question = create(:question, user: user)
+
+      expect(user.owes_resource(question)).to be_truthy
+    end
+
+    it "compares invalid user with resource owner" do
+      another_user = create(:user)
+      question = create(:question, user: another_user)
+
+      expect(user.owes_resource(question)).to be_falsey
+    end
+  end
 end
