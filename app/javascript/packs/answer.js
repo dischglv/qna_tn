62066069e0dbf4b33db753d47f3666b9592e21b9
Diana Shchegoleva <dschegoleva@gmail.com>
2021-02-10 21:17:21 +0300

    Add AJAX to answer edit

--- app/javascript/packs/answer.js
+++ app/javascript/packs/answer.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-  $('.edit-answer-link').on('click', function(e) {
+$(document).on('turbolinks:load', function() {
+  $('.answers').on('click', '.edit-answer-link', function(e) {
     e.preventDefault();
     $(this).hide();
     var answerId = $(this).data('answerId');
-    $('form#edit-answer-' + answerId).show();
-  }
+    $('form#edit-answer-' + answerId).removeClass('hidden');
+  })
 });
