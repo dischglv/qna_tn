0e6f364c83809ba2bfc7ca531e94bc9d85b23739
Diana Shchegoleva <dschegoleva@gmail.com>
2021-02-24 13:40:30 +0300

    Add URL validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,14 @@
+require 'uri'
+
 class Link < ApplicationRecord
-  belongs_to :linkable, polymorphic: true
+  belongs_to :linkable, polymorphic: true, optional: true
 
   validates :name, :url, presence: true
+  validate :validates_url_format
+
+  def validates_url_format
+    URI.parse(url)
+  rescue URI::InvalidURIError
+    errors.add(:url, 'url must be valid')
+  end
 end
